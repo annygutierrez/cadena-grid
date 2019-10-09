835d5620525451b09fad1a7f3d06750ce9bc6f35
Anny Fiorella Gutierrez Lopez <agutilop@DONT-PANIC.local>
2019-10-09 18:04:21 -0500

    Add grid first version
    - Add free filters and new style to the predetermined theme.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import YearFilter from './stringExtensions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,100 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cadena-grid';
+  title = 'app';
+
+    columnDefs = [
+        {
+          headerName: 'USER',
+          field: 'user',
+          width: 140,
+          cellStyle: {
+            fontWeight: 'bold',
+            color: '#333333'
+          },
+          filter: 'agTextColumnFilter',
+          filterParams: { applyButton: false, clearButton: false }
+        },
+        {headerName: 'NAME AND LAST NAME', field: 'name', filter: 'agTextColumnFilter',
+        filterParams: { applyButton: true, clearButton: true } },
+        {headerName: 'ROLE', field: 'role', width: 340, filter: 'agTextColumnFilter',
+        filterParams: { applyButton: true, clearButton: true }},
+        {headerName: 'STATUS', field: 'status', cellRenderer: (params) => {
+          console.log(params);
+          // check the data exists, to avoid error
+          if (params.value === 'approved') {
+              // data exists, so we can access it
+              return '<div>' + '<span style="background-color: #27ae60; padding: 0.6em 1.2em; border-radius: 20px; color: white">' + params.data.status + '</span>' + '</div>';
+          } else if (params.value === 'created') {
+              // when we return null, the grid will display a blank cell
+              return '<div>' + '<span style="background-color: #f2c94c; padding: 0.6em 1.2em; border-radius: 20px; color: white">' + params.data.status + '</span>' + '</div>';
+          } else if (params.value === 'disabled') {
+            // when we return null, the grid will display a blank cell
+            return '<div>' + '<span style="background-color: #BDBDBD; padding: 0.6em 1.2em; border-radius: 20px; color: white">' + params.data.status + '</span>' + '</div>';
+        } else if (params.value === 'rejected') {
+          // when we return null, the grid will display a blank cell
+          return '<div>' + '<span style="background-color: #eb5757; padding: 0.6em 1.2em; border-radius: 20px; color: white">' + params.data.status + '</span>' + '</div>';
+      }
+      },
+      filter: YearFilter
+    },
+      {headerName: '', field: 'more', cellRenderer: (params) => {
+        console.log(params);
+            return '<div>' + '<span style="color: #333333; font-weight: bold; cursor: pointer">' + 'Ver mas' + '</span>' + '</div>';
+        }
+      }
+    ];
+
+    irishAthletes() {
+      return [
+          'approved',
+          'rejected',
+          'created',
+          'disabled'
+      ];
+  }
+
+    myAlert() {
+      alert('ver mas');
+    }
+
+    onCellClicked($event) {
+      console.log($event);
+      if ($event.colDef.field === 'more') {
+        alert('MORE ABOUT: ' + $event.data.name);
+      }
+  }
+    // defaultColDef = {
+      // set every column width
+      // width: 280
+      // make every column editable
+      // editable: true,
+      // make every column use 'text' filter by default
+      // filter: 'agTextColumnFilter'
+  // };
+
+  // gridOptions = {
+  //   rowClassRules: {
+        // row style function
+        // 'sick-days-warning': function(params) {
+        //     var numSickDays = params.data.sickDays;
+        //     return  numSickDays > 5 && numSickDays <= 7;
+        // },
+        // row style expression
+//         'sick-days-breach': "data.status === 'approved'"
+//     }
+// };
+
+    rowData = [
+        { user: 'agutierrez', role: 'Usuario de consulta de entidad privada', name: 'Andrea Gutierrez', status: 'approved' },
+        { user: 'ccarrillo', role: 'Administrador de usuarios Cadena', name: 'Carlos Carrillo', status: 'approved' },
+        { user: 'chenao', role: 'Registrador de certificados', name: 'Chela Naori', status: 'disabled' },
+        { user: 'lleopardi', role: 'Aprobador de registros', name: 'Leonardo Leopardi', status: 'created' },
+        { user: 'epacheco', role: 'Usuario de consulta de entidad pública', name: 'Elias Pacheco', status: 'created' },
+        { user: 'macosta', role: 'Usuario de consulta de entidad pública', name: 'Elias Pacheco', status: 'rejected' },
+        { user: 'dhuaman', role: 'Usuario de consulta de entidad privada', name: 'Diego Huaman', status: 'created' },
+        { user: 'jeijei', role: 'Usuario de consulta de entidad pública', name: 'Jevert Jahawanca', status: 'approved' },
+        { user: 'projas', role: 'Registrador de certificados', name: 'Peter Rojas', status: 'approved' },
+        { user: 'rbennet', role: 'Usuario de consulta de entidad privada', name: 'Rebeca Bennet', status: 'approved' }
+    ];
 }
---
+++ src/app/stringExtensions.ts
@@ -0,0 +1,112 @@
+
+
+  class YearFilter {
+    eGui;
+    rbAllYears;
+    rbSince2010;
+    filterActive;
+    filterChangedCallback;
+    valueGetter;
+    onRbChanged = () => {
+      this.filterActive = this.rbSince2010.checked;
+      this.filterChangedCallback();
+    }
+
+    init = (params) => {
+      this.eGui = document.createElement('div');
+      this.eGui.innerHTML =
+          '<div style="display: inline-block; width: 400px;">' +
+          '<div style="padding: 10px; background-color: #d3d3d3; text-align: center;">' +
+          'This is a very wide filter' +
+          '</div>'+
+          '<label style="margin: 10px; padding: 50px; display: inline-block; background-color: #999999">'+
+          '  <input type="radio" name="yearFilter" checked="true" id="rbAllYears" filter-checkbox="true"/> All'+
+          '</label>'+
+          '<label style="margin: 10px; padding: 50px; display: inline-block; background-color: #999999">'+
+          '  <input type="radio" name="yearFilter" id="rbSince2010" filter-checkbox="true"/> Since 2010'+
+          '</label>' +
+          '</div>';
+      this.rbAllYears = this.eGui.querySelector('#rbAllYears');
+      this.rbSince2010 = this.eGui.querySelector('#rbSince2010');
+      this.rbAllYears.addEventListener('change', this.onRbChanged.bind(this));
+      this.rbSince2010.addEventListener('change', this.onRbChanged.bind(this));
+      this.filterActive = false;
+      this.filterChangedCallback = params.filterChangedCallback;
+      this.valueGetter = params.valueGetter;
+    }
+
+    getGui = () => {
+      return this.eGui;
+    }
+
+    doesFilterPass = (params) => {
+      return params.data.year >= 2010;
+    }
+
+    isFilterActive = () => {
+      return this.filterActive;
+    }
+
+    getModel = function() {
+      return {value: this.rbSince2010.checked};
+    };
+
+    setModel = function(model) {
+      this.rbSince2010.checked = model.value;
+    };
+  }
+
+
+// YearFilter.prototype.init = (params) => {
+//   this.eGui = document.createElement('div');
+//   this.eGui.innerHTML =
+//       '<div style="display: inline-block; width: 400px;">' +
+//       '<div style="padding: 10px; background-color: #d3d3d3; text-align: center;">' +
+//       'This is a very wide filter' +
+//       '</div>'+
+//       '<label style="margin: 10px; padding: 50px; display: inline-block; background-color: #999999">'+
+//       '  <input type="radio" name="yearFilter" checked="true" id="rbAllYears" filter-checkbox="true"/> All'+
+//       '</label>'+
+//       '<label style="margin: 10px; padding: 50px; display: inline-block; background-color: #999999">'+
+//       '  <input type="radio" name="yearFilter" id="rbSince2010" filter-checkbox="true"/> Since 2010'+
+//       '</label>' +
+//       '</div>';
+//   this.rbAllYears = this.eGui.querySelector('#rbAllYears');
+//   this.rbSince2010 = this.eGui.querySelector('#rbSince2010');
+//   this.rbAllYears.addEventListener('change', this.onRbChanged.bind(this));
+//   this.rbSince2010.addEventListener('change', this.onRbChanged.bind(this));
+//   this.filterActive = false;
+//   this.filterChangedCallback = params.filterChangedCallback;
+//   this.valueGetter = params.valueGetter;
+// };
+
+// YearFilter.prototype.onRbChanged = function () {
+//   this.filterActive = this.rbSince2010.checked;
+//   this.filterChangedCallback();
+// };
+
+// YearFilter.prototype.getGui = () => {
+//   return this.eGui;
+// };
+
+// YearFilter.prototype.doesFilterPass = (params) => {
+//   return params.data.year >= 2010;
+// };
+
+// YearFilter.prototype.isFilterActive = () => {
+//   return this.filterActive;
+// };
+
+// YearFilter.prototype.getModel = function() {
+//   return {value: this.rbSince2010.checked};
+// };
+
+// YearFilter.prototype.setModel = function(model) {
+//   this.rbSince2010.checked = model.value;
+// };
+
+// this example isn't using getModel() and setModel(),
+// so safe to just leave these empty. don't do this in your code!!!
+YearFilter.prototype.getModel = () => {};
+YearFilter.prototype.setModel = () => {};
+
